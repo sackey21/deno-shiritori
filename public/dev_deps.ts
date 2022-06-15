6e96a8617fa12918fd401f03123f6611fab9ba5b
homosapico <sackey.sg@gmail.com>
2022-06-16 02:12:34 +0900

    faviconの追加，tsへの移行，module管理用dev_deps.ts作成

---
+++ public/dev_deps.ts
@@ -0,0 +1,2 @@
+export { serve } from "https://deno.land/std@0.138.0/http/server.ts";
+export { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
--- server.ts
+++ server.ts
@@ -1,5 +1,5 @@
-import { serve } from "https://deno.land/std@0.138.0/http/server.ts";
-import { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
+import { serve } from "./public/dev_deps.ts";
+import { serveDir } from "./public/dev_deps.ts";
 
 let previousWord = "しりとり";
 
