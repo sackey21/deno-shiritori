011b1cc187baa124b37ff80effd85b7455613eba
homosapico <sackey.sg@gmail.com>
2022-06-17 02:43:59 +0900

    ひらがなチェックとランダムスタート実装，servest諦め

--- public/dev_deps.ts
+++ public/dev_deps.ts
@@ -1,2 +1,3 @@
+
 export { serve } from "https://deno.land/std@0.138.0/http/server.ts";
-export { serveDir } from "https://deno.land/std@0.138.0/http/file_server.ts";
+export { serveDir,  } from "https://deno.land/std@0.138.0/http/file_server.ts";
--- server.ts
+++ server.ts
@@ -1,27 +1,33 @@
 import { serve } from "./public/dev_deps.ts";
 import { serveDir } from "./public/dev_deps.ts";
 
-let previousWord = "しりとり";
 
+let previousWord = randomStart();
 console.log("Listening on http://localhost:8000");
+
 serve(async (req) => {
-  const pathname = new URL(req.url).pathname;
+  const pathname = new URL(req.url).pathname
   if (req.method === "GET" && pathname === "/shiritori") {
     return new Response(previousWord);
   }
-  
   if (req.method === "POST" && pathname === "/shiritori") {
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
+
+    if(!HiraganaCheck(nextWord)) {
+      return new Response("ひらがなで入力してください", { status: 400 });
+    }
+
     if (
       nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)
     ) {
-      return new Response("前の単語に続いていません。", { status: 400 });
+      return new Response("前の単語に続いていません", { status: 400 });
     }
     
     //終了時，最初の単語で初期化
     if ( nextWord.charAt(nextWord.length - 1) === 'ん' ) {
-      return new Response("しりとり");
+      previousWord = randomStart();
+      return new Response(previousWord);
     }
     
     previousWord = nextWord;
@@ -35,5 +41,23 @@ serve(async (req) => {
     showDirListing: true,
     enableCors: true,
   });
-
 });
+
+function randomStart(){
+  const str = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふへほまみむめもやゆよらりるれろわがぎぐげござじずぜぞだぢづでどばびぶべぼぱぴぷぺぽ";
+  let num = Math.floor(Math.random() * str.length); + 1;
+  let previousWord = str.charAt(num);
+  return previousWord;
+}
+
+function HiraganaCheck(str: string){
+  let ch: number;
+  for(let i = 0; i < str.length; i++){
+    ch = str.charCodeAt(i);
+
+    if(ch < 12343 || ch > 12438){
+      return false;
+    }
+  }
+  return true;
+}
