f1e3afffb709ac6baf1a678805dc8880469bcba3
homosapico <sackey.sg@gmail.com>
2022-06-20 01:11:33 +0900

    履歴の表示実装

--- server.ts
+++ server.ts
@@ -1,38 +1,50 @@
 import { serve } from "./public/dev_deps.ts";
 import { serveDir } from "./public/dev_deps.ts";
+import "https://deno.land/x/xhr@0.1.0/mod.ts";
 
-
+let words = new Array<string>();
 let previousWord = randomStart();
 console.log("Listening on http://localhost:8000");
 
 serve(async (req) => {
   const pathname = new URL(req.url).pathname
+
   if (req.method === "GET" && pathname === "/shiritori") {
-    return new Response(previousWord);
+    reset();
+    return new Response(JSON.stringify(words));
   }
   if (req.method === "POST" && pathname === "/shiritori") {
     const requestJson = await req.json();
     const nextWord = requestJson.nextWord;
+    
+    if(!(words instanceof Array)){
+      return new Response("単語のlogが残りません", { status: 400 });
+    }
 
     if(!HiraganaCheck(nextWord)) {
       return new Response("ひらがなで入力してください", { status: 400 });
     }
-
     if (
       nextWord.length > 0 && previousWord.charAt(previousWord.length - 1) !== nextWord.charAt(0)
     ) {
       return new Response("前の単語に続いていません", { status: 400 });
     }
     
+    if (
+      nextWord.length < 0 
+    ) {
+      return new Response("入力してください", { status: 400 });
+    }
+
     //終了時，最初の単語で初期化
     if ( nextWord.charAt(nextWord.length - 1) === 'ん' ) {
-      previousWord = randomStart();
-      return new Response(previousWord);
+      reset();
+      return new Response(JSON.stringify(words), { status: 220 });
     }
-    
-    previousWord = nextWord;
-    return new Response(previousWord);
 
+    words.push(nextWord);
+    previousWord = nextWord;
+    return new Response(JSON.stringify(words));
   }
 
   return serveDir(req, {
@@ -60,4 +72,10 @@ function HiraganaCheck(str: string){
     }
   }
   return true;
+}
+
+function reset() {
+  words.splice(0);
+  previousWord = randomStart();
+  words.push(previousWord);
 }
