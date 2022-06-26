bc17af30bdeb2e14863b20cb642e2aa8be8affb3
homosapico <sackey.sg@gmail.com>
2022-06-26 22:02:07 +0900

    エラー修正

--- public/dev_deps.ts
+++ public/dev_deps.ts
@@ -1,6 +1,6 @@
 export * as xhr from "https://deno.land/x/xhr@0.1.0/mod.ts" ;//websocketできたら
 export { serve } from "https://deno.land/std@0.138.0/http/server.ts";//
-export { serveDir,  } from "https://deno.land/std@0.138.0/http/file_server.ts";//
+export { serveDir} from "https://deno.land/std@0.138.0/http/file_server.ts";//
 export { parse } from "https://deno.land/std@0.79.0/encoding/csv.ts";    
 export { readLines } from 'https://deno.land/std/io/mod.ts';
 
